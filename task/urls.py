911aeaf0aef53682ac580396b70a08bf0a31cc15
foy4748 <faisaljfcl@gmail.com>
2024-10-01 03:55:56 +0600

    Added Class-Based views in task app. Lost the message feedback feature

--- task/urls.py
+++ task/urls.py
@@ -3,8 +3,14 @@ from . import views
 
 app_name = "task"
 urlpatterns = [
-    path("", views.AllTasks, name="show_tasks"),
-    path("add_task/", views.ShowAddTaskForm, name="add_task_form"),
-    path("edit_task/<str:pk>/", views.ShowEditTaskForm, name="edit_task_form"),
-    path("delete_task/<str:pk>/", views.DeleteSingleTask, name="delete_task"),
+    # path("", views.AllTasks, name="show_tasks"),
+    path("", views.AllTasks.as_view(), name="show_tasks"),
+    # path("add_task/", views.ShowAddTaskForm, name="add_task_form"),
+    path("add_task/", views.ShowAddTaskForm.as_view(), name="add_task_form"),
+    # path("edit_task/<str:pk>/", views.ShowEditTaskForm, name="edit_task_form"),
+    path(
+        "edit_task/<str:pk>/", views.ShowEditTaskForm.as_view(), name="edit_task_form"
+    ),
+    # path("delete_task/<str:pk>/", views.DeleteSingleTask, name="delete_task"),
+    path("delete_task/<str:pk>/", views.DeleteSingleTask.as_view(), name="delete_task"),
 ]
--- task/views.py
+++ task/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import get_object_or_404, redirect, render
-from django.contrib import messages
+# from django.urls import reverse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+# from django.shortcuts import get_object_or_404, redirect, render
+# from django.contrib import messages
+from django.views.generic.edit import DeleteView, UpdateView
+from django.views.generic.list import ListView
 
 from task.forms import AddTaskForm
 from task.models import TaskModel
@@ -7,52 +13,82 @@ from task.models import TaskModel
 # Create your views here.
 
 
-def AllTasks(req):
-    tasks = TaskModel.objects.all()
-    return render(req, "task/task.html", {"tasks": tasks})
+class AllTasks(ListView):
+    model = TaskModel
 
 
-def ShowAddTaskForm(req):
-    if req.POST:
-        print("POSTED")
-        current_form = AddTaskForm(req.POST)
-        if current_form.is_valid():
-            current_form.save()
-            success_message = (
-                f"Added new task {current_form.cleaned_data.get('task_title')}"
-            )
-
-            messages.success(req, success_message)
-            return redirect("task:show_tasks")
-        else:
-            error_message = "Given task can't be stored"
-            messages.error(req, error_message)
-            return redirect("task:show_tasks")
-    form = AddTaskForm()
-    return render(req, "task/addTaskForm.html", {"form": form})
+class ShowAddTaskForm(CreateView):
+    model = TaskModel
+    template_name = "task/addTaskForm.html"
+    form_class = AddTaskForm
+    # Doesn't work
+    # success_url = reverse("task:show_tasks")
+    success_url = reverse_lazy("task:show_tasks")
 
 
-def ShowEditTaskForm(req, pk):
-    task_instance = get_object_or_404(TaskModel, pk=pk)
-    if req.POST:
-        print("POSTED")
-        current_form = AddTaskForm(req.POST, instance=task_instance)
-        if current_form.is_valid():
-            current_form.save()
-            success_message = (
-                f"Updated task {current_form.cleaned_data.get('task_title')}"
-            )
-            messages.success(req, success_message)
-            return redirect("task:show_tasks")
-        else:
-            error_message = "Given task can't be stored"
-            messages.error(req, error_message)
-            return redirect("task:show_tasks")
-    form = AddTaskForm(instance=task_instance)
-    return render(req, "task/addTaskForm.html", {"form": form})
+class ShowEditTaskForm(UpdateView):
+    model = TaskModel
+    template_name = "task/addTaskForm.html"
+    form_class = AddTaskForm
+    # Doesn't work
+    # success_url = reverse("task:show_tasks")
+    success_url = reverse_lazy("task:show_tasks")
 
 
-def DeleteSingleTask(_, pk):
-    task_instance = get_object_or_404(TaskModel, pk=pk)
-    task_instance.delete()
-    return redirect("task:show_tasks")
+class DeleteSingleTask(DeleteView):
+    model = TaskModel
+    success_url = reverse_lazy("task:show_tasks")
+
+
+########
+
+# function based views ===================
+
+# def AllTasks(req):
+#     tasks = TaskModel.objects.all()
+#     return render(req, "task/task.html", {"tasks": tasks})
+
+# def ShowAddTaskForm(req):
+#     if req.POST:
+#         print("POSTED")
+#         current_form = AddTaskForm(req.POST)
+#         if current_form.is_valid():
+#             current_form.save()
+#             success_message = (
+#                 f"Added new task {current_form.cleaned_data.get('task_title')}"
+#             )
+
+#             messages.success(req, success_message)
+#             return redirect("task:show_tasks")
+#         else:
+#             error_message = "Given task can't be stored"
+#             messages.error(req, error_message)
+#             return redirect("task:show_tasks")
+#     form = AddTaskForm()
+#     return render(req, "task/addTaskForm.html", {"form": form})
+
+
+# def ShowEditTaskForm(req, pk):
+#     task_instance = get_object_or_404(TaskModel, pk=pk)
+#     if req.POST:
+#         print("POSTED")
+#         current_form = AddTaskForm(req.POST, instance=task_instance)
+#         if current_form.is_valid():
+#             current_form.save()
+#             success_message = (
+#                 f"Updated task {current_form.cleaned_data.get('task_title')}"
+#             )
+#             messages.success(req, success_message)
+#             return redirect("task:show_tasks")
+#         else:
+#             error_message = "Given task can't be stored"
+#             messages.error(req, error_message)
+#             return redirect("task:show_tasks")
+#     form = AddTaskForm(instance=task_instance)
+#     return render(req, "task/addTaskForm.html", {"form": form})
+
+
+# def DeleteSingleTask(_, pk):
+#     task_instance = get_object_or_404(TaskModel, pk=pk)
+#     task_instance.delete()
+#     return redirect("task:show_tasks")
