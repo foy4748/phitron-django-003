1075e832defc269111aa1d62f5e9a2aa59bf8b20
foy4748 <faisaljfcl@gmail.com>
2024-10-01 13:36:39 +0600

    Rendering notification within the Class-Based views

--- task/views.py
+++ task/views.py
@@ -1,9 +1,10 @@
 # from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 # from django.shortcuts import get_object_or_404, redirect, render
-# from django.contrib import messages
+from django.contrib import messages
 from django.views.generic.edit import DeleteView, UpdateView
 from django.views.generic.list import ListView
 
@@ -25,6 +26,18 @@ class ShowAddTaskForm(CreateView):
     # success_url = reverse("task:show_tasks")
     success_url = reverse_lazy("task:show_tasks")
 
+    # Thanks to this StackOverflow page
+    # https://stackoverflow.com/questions/32998300/django-createview-how-to-perform-action-upon-save
+
+    # Showing Success message
+    # for successful submission
+    def form_valid(self, form):
+        self.object = form.save()
+        success_message = f"Added new task {form.cleaned_data.get('task_title')}"
+
+        messages.success(self.request, success_message)
+        return HttpResponseRedirect(self.get_success_url())
+
 
 class ShowEditTaskForm(UpdateView):
     model = TaskModel
@@ -34,10 +47,33 @@ class ShowEditTaskForm(UpdateView):
     # success_url = reverse("task:show_tasks")
     success_url = reverse_lazy("task:show_tasks")
 
+    # Thanks to this StackOverflow page
+    # https://stackoverflow.com/questions/32998300/django-createview-how-to-perform-action-upon-save
+
+    # Showing Success message
+    # for successful submission
+    def form_valid(self, form):
+        self.object = form.save()
+        success_message = f"Added new task {form.cleaned_data.get('task_title')}"
+
+        messages.success(self.request, success_message)
+        return HttpResponseRedirect(self.get_success_url())
+
 
 class DeleteSingleTask(DeleteView):
     model = TaskModel
     success_url = reverse_lazy("task:show_tasks")
+
+    # Thanks Copilot
+    # Also, def delete() won't work
+    # Only Copilot wasn't enough 🙂
+    def post(self, request, *args, **kwargs):
+        todo = self.get_object()
+        # print(todo.task_title, todo.id)
+        success_message = f"Deleted task {todo.task_title} with id {todo.id}"
+
+        messages.success(self.request, success_message)
+        return super().post(request, *args, **kwargs)
 
 
 ########
